d8ec4e383ad60a746c0b3be50b4a998f7db96528
grohit1810 <57721205+grohit1810@users.noreply.github.com>
2019-11-16 15:53:17 +0000

    Remove redundant code

--- src/json_reader.py
+++ src/json_reader.py
@@ -7,10 +7,6 @@ Created on Fri Nov 15 16:02:38 2019
 
 import json
 import pickle
-
-json_file = open("c8f0f002.json")
-data = json.load(json_file)
-#print(data['train'][0]['output'])
 '''
 d = {}
 d["train1"] = data['train'][0]['output']
@@ -19,13 +15,6 @@ d["train3"] = data['train'][0]['output']
 d["test1"] = data['test'][0]['output']
 
 pickle.dump( d, open( "output_format.pkl", "wb" ) )'''
-
-train_inputs = [data['train'][i]['input'] for i in range(len(data['train']))]
-test_inputs = [data['test'][i]['input'] for i in range(len(data['test']))]
-train_outputs = [data['train'][i]['output'] for i in range(len(data['train']))]
-test_outputs = [data['test'][i]['output'] for i in range(len(data['test']))]
-
-
 
 def ARCTaskJsonReader(json_filename):
     json_file = open(json_filename)
