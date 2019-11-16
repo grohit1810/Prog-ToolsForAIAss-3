d66924bccb4fe611b6553b01c2c95d6c5a0ba96e
grohit1810 <57721205+grohit1810@users.noreply.github.com>
2019-11-16 15:51:11 +0000

    Add function to read Json

--- src/json_reader.py
+++ src/json_reader.py
@@ -24,3 +24,16 @@ train_inputs = [data['train'][i]['input'] for i in range(len(data['train']))]
 test_inputs = [data['test'][i]['input'] for i in range(len(data['test']))]
 train_outputs = [data['train'][i]['output'] for i in range(len(data['train']))]
 test_outputs = [data['test'][i]['output'] for i in range(len(data['test']))]
+
+
+
+def ARCTaskJsonReader(json_filename):
+    json_file = open(json_filename)
+    data = json.load(json_file)
+    
+    train_inputs = [data['train'][i]['input'] for i in range(len(data['train']))]
+    test_inputs = [data['test'][i]['input'] for i in range(len(data['test']))]
+    train_outputs = [data['train'][i]['output'] for i in range(len(data['train']))]
+    test_outputs = [data['test'][i]['output'] for i in range(len(data['test']))]
+    
+    return (train_inputs,train_outputs,test_inputs,test_outputs)
