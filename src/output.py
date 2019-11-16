a3a7683e5936f1415d6342e9e61a5f1a5df4355d
Mani Vegupatti Selvanathan <m.vegupattiselvanathan1@nuigalway.ie>
2019-11-16 18:23:10 +0530

    Update output.py

--- src/output.py
+++ src/output.py
@@ -3,9 +3,27 @@
 Created on Sat Nov 16 04:23:26 2019
 
 @author: Mani Vegupatti
-"""
 
-import pickle
+This will print the output of the solver to terminal
+>>> dict_data = { "TRAIN1": [[1, 8, 8, 5, 5, 8], [1, 1, 5, 5, 1, 8], [5, 1, 1, 5, 5, 8]], "TRAIN2": [[1, 8, 1, 5, 1, 2], [5, 8, 8, 1, 1, 3], [5, 1, 8, 8, 5, 5]], "TEST1": [[2, 5, 6, 5, 3, 1], [4, 3, 2, 6, 9, 5], [1, 3, 5, 7, 9, 4]] }
+>>> print_grid(dict_data)
+1 8 8 5 5 8 
+1 1 5 5 1 8 
+5 1 1 5 5 8 
+<BLANKLINE>
+1 8 1 5 1 2 
+5 8 8 1 1 3 
+5 1 8 8 5 5 
+<BLANKLINE>
+2 5 6 5 3 1 
+4 3 2 6 9 5 
+1 3 5 7 9 4 
+<BLANKLINE>
+
+Sample from pickle 
+d = pickle.load(open("output_format.pkl","rb"))
+print_grid(d)        
+"""
 
 
 
@@ -14,9 +32,10 @@ def print_grid(output):
         for grid in (output[key]):
             for element in grid:
                 print(element, end=" ")
-            print("\n")
-        print("----\n")
+            print("")
+        print("")
         
-
-d = pickle.load(open("output_format.pkl","rb"))
-print_grid(d)        
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    
