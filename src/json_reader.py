2988718b88023c1eb68772fb476c725f01158911
grohit1810 <57721205+grohit1810@users.noreply.github.com>
2019-11-16 15:45:00 +0000

    Add code to read from json

--- src/json_reader.py
+++ src/json_reader.py
@@ -11,10 +11,16 @@ import pickle
 json_file = open("c8f0f002.json")
 data = json.load(json_file)
 #print(data['train'][0]['output'])
+'''
 d = {}
 d["train1"] = data['train'][0]['output']
 d["train2"] = data['train'][2]['output']
 d["train3"] = data['train'][0]['output']
 d["test1"] = data['test'][0]['output']
 
-pickle.dump( d, open( "output_format.pkl", "wb" ) )
+pickle.dump( d, open( "output_format.pkl", "wb" ) )'''
+
+train_inputs = [data['train'][i]['input'] for i in range(len(data['train']))]
+test_inputs = [data['test'][i]['input'] for i in range(len(data['test']))]
+train_outputs = [data['train'][i]['output'] for i in range(len(data['train']))]
+test_outputs = [data['test'][i]['output'] for i in range(len(data['test']))]
