cff95c9d11aab34987c0f2305cbc5660b0bf4a44
grohit1810 <57721205+grohit1810@users.noreply.github.com>
2019-11-21 13:07:01 +0000

    Update solution_c2f0f002.py to make the solve function look more cleaner

--- src/solution_c8f0f002.py
+++ src/solution_c8f0f002.py
@@ -8,10 +8,12 @@ Created on Sat Nov 16 18:48:54 2019
 import sys 
 from io_functions import arc_task_json_reader
 from output import print_grid
+import numpy as np
 
 #the input is python list of lists
 def solve_c8f0f002_json(input):
     
+    '''
     input_copy = input.copy()
     for li in input_copy:
         for index_element in range(len(li)):
@@ -19,6 +21,8 @@ def solve_c8f0f002_json(input):
                 li[index_element] = 5
     
     return input_copy
+    '''
+    return(np.where(input!=7,input,5).tolist())
 
 if __name__ == '__main__':
     
@@ -29,10 +33,12 @@ if __name__ == '__main__':
     train_inputs,train_outputs,test_inputs,test_outputs = arc_task_json_reader(sys.argv[1])
     print_dict = {}
     for index in range(len(train_inputs)) :
-        output = solve_c8f0f002_json(train_inputs[index])
+        output = solve_c8f0f002_json(np.array(train_inputs[index]))
         print_dict['TRAIN'+str(index)] = output
+        #print_dict['TRAIN1'+str(index)] = train_outputs[index]
     for index in range(len(test_inputs)) : 
-        output = solve_c8f0f002_json(test_inputs[index])
+        output = solve_c8f0f002_json(np.array(test_inputs[index]))
         print_dict['TEST'+str(index)] = output
+        #print_dict['TEST1'+str(index)] = test_outputs[index]
 
     print_grid(print_dict)
