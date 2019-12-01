9af0a814ce66fa39e8bfca5777ddb016157b5988
Mani Vegupatti Selvanathan <m.vegupattiselvanathan1@nuigalway.ie>
2019-12-01 20:11:20 +0530

    Update of IO Functions
    Updated Print Grid
    Updated Comments and DocString

--- src/io_functions.py
+++ src/io_functions.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 16 18:44:21 2019
+"""Input/Ouput functions for ARC task.
 
+<<<<<<< Updated upstream
 @author: 19233292
 @author: 19232668, Mani Vegupatti
+=======
+It contains 2 functions arc_json_reader and print_grid
+>>>>>>> Stashed changes
 
 Function print_grid will print the output of the solver to terminal
 >>> dict_data = { "TRAIN1": [[1, 8, 8, 5, 5, 8], [1, 1, 5, 5, 1, 8], [5, 1, 1, 5, 5, 8]], "TRAIN2": [[1, 8, 1, 5, 1, 2], [5, 8, 8, 1, 1, 3], [5, 1, 8, 8, 5, 5]], "TEST1": [[2, 5, 6, 5, 3, 1], [4, 3, 2, 6, 9, 5], [1, 3, 5, 7, 9, 4]] }
@@ -21,11 +24,19 @@ Function print_grid will print the output of the solver to terminal
 1 3 5 7 9 4 
 <BLANKLINE>
 
-
+Created on Sat Nov 16 18:44:21 2019
+@author: 19233292, G Rohit
+@author: 19232668, Mani Vegupatti
 """
+
 import json
 
+<<<<<<< Updated upstream
 def json_reader(json_filename):
+=======
+def arc_json_reader(json_filename):
+    """Reader function to read and parse ARC task Json file."""
+>>>>>>> Stashed changes
     json_file = open(json_filename)
     data = json.load(json_file)
     
@@ -37,14 +48,17 @@ def json_reader(json_filename):
     return (train_inputs,train_outputs,test_inputs,test_outputs)
 
 def print_grid(output):
-    for key in output:
-        for grid in (output[key]):
-            for element in grid:
-                print(element, end=" ")
+    """Print function to print the ARC task output of solver to console."""
+    for key in output: # Get the grid
+        for row in (output[key]):# Get the row
+            for element in row: 
+                print(element, end=" ")  # Print element, stay in same line
             print()
         print()
         
 if __name__ == "__main__":
+    
+    #import doctest and test
     import doctest
     doctest.testmod()
     
